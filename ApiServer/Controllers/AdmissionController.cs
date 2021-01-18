86aba1c8faf237690b9479c17738ca2c03da242a
unknown <khoaimiday@yahoo.com>
2021-01-18 23:30:41 +0700

    edit GetInfoAdmission

--- ApiServer/Controllers/AdmissionController.cs
+++ ApiServer/Controllers/AdmissionController.cs
@@ -27,7 +27,7 @@ namespace APIServer.Controllers
         public async Task<Object> SubmitAdmission(tbAdmission model)
         {
             //Student apply dupplicate admission
-            var mailChk = db.Admissions.SingleOrDefault(e => e.Equals(model.StudentEmail));
+            var mailChk = db.Admissions.SingleOrDefault(e => e.StudentEmail.Equals(model.StudentEmail));
             if (mailChk != null)
             {
                 if (mailChk.FieldCode.Equals(model.FieldCode))
@@ -92,6 +92,52 @@ namespace APIServer.Controllers
                 return BadRequest("Admission ID cannot null!");
             }
             var Admission = await db.Admissions.FindAsync(Id);
+            //SpecializedSubject
+            var SpecSubject = db.SpeSubjects.SingleOrDefault(e => e.SubjectID.Equals(Admission.SpecializedSubjectID));
+            if (SpecSubject != null)
+            {
+                Admission.tbSpeSubject = SpecSubject;
+            }
+            //OptionalSubject
+            var OpSubject = db.OpSubjects.SingleOrDefault(e => e.SubjectID.Equals(Admission.OptionalSubjectID));
+            if (OpSubject != null)
+            {
+                Admission.tbOpSubject = OpSubject;
+            }
+            //AdmissionStatus
+            var AdmisStatus = db.AdmissionStatuses.SingleOrDefault(e => e.StatusID == Admission.StatusID);
+            if (AdmisStatus != null)
+            {
+                Admission.tbAdmissionStatus = AdmisStatus;
+            }
+            //Field
+            var Field = db.Fields.SingleOrDefault(e => e.FieldCode.Equals(Admission.FieldCode));
+            if (Field != null)
+            {
+                //Course
+                var CourseList = db.Courses.Where(x => x.FieldCode.Equals(Field.FieldCode)).ToList();
+                if (CourseList != null)
+                {
+                    Field.TbCourses = CourseList;
+                }
+                //Subject
+                var SubjectList = db.SpeSubjects.Where(x => x.FieldCode.Equals(Field.FieldCode)).ToList();
+                if(SubjectList != null)
+                {
+                    Field.tbSpeSubjects = SubjectList;
+                }
+                Admission.tbField = Field;
+            }
+            //Stream 
+            var Stream = db.Streams.SingleOrDefault(e => e.StreamCode.Equals(Admission.StreamCode));
+            if (Stream != null)
+            {
+                //Field
+                var FieldList = db.Fields.Where(x => x.StreamCode.Equals(Stream.StreamCode)).ToList();
+                Stream.TbFields = FieldList;
+                Admission.tbStream = Stream;
+            }
+
             return new { Admission };
         }
 
